61e602ec3f9e2075146e2ec5df2ff7e2c6892298
Matt <contractdeveloper0@gmail.com>
2024-11-13 12:57:00 +0200

    update: connect db using global

--- config/db.js
+++ config/db.js
@@ -1,23 +1,52 @@
-import mongoose from 'mongoose';
+import mongoose, { connect } from 'mongoose';
 
-const uri = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
-export const connectDB = async () => {
-  if (mongoose.connection.readyState === 0) {
-    try {
-      console.log('Connecting to MongoDB...');
-      await mongoose.connect(uri);
-      console.log('MongoDB connected');
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-      throw new Error('Unable to connect to MongoDB');
-    }
+if (!MONGO_URI || MONGO_URI.length === 0) {
+  throw new Error("MONGO_URI not found");
+}
+
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
+}
+
+let cached = global.mongoose;
+
+export const connectDB = async function () {
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = connect(MONGO_URI, opts)
+      .then((mongoose) => {
+        console.log("✅ New connection established");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("❌ Connection to database failed");
+        throw error;
+      });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 };
 
 export const disconnectDB = async () => {
-  if (mongoose.connection.readyState !== 0) {
+  if (cached.conn) {
     await mongoose.disconnect();
     console.log('MongoDB disconnected');
+    cached.conn = null;
   }
 };
